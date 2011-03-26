2382d65285371d8197c8edf938995650f075b9b9
Benjamin Floering <floering@ieee.org>
2011-03-26 12:47:18 -0700

    a bit of robustness for creating from session keys

--- lib/mogli/client.rb
+++ lib/mogli/client.rb
@@ -70,8 +70,12 @@ module Mogli
     def self.create_from_session_key(session_key, client_id, secret)
       authenticator = Mogli::Authenticator.new(client_id, secret, nil)
       access_data = authenticator.get_access_token_for_session_key(session_key)
-      new(access_data.first['access_token'],
-          Time.now.to_i + access_data.first['expires'].to_i)
+      if access_data.first
+        new(access_data.first['access_token'],
+            Time.now.to_i + access_data.first['expires'].to_i)
+      else
+        nil
+      end
     end
 
     def post(path,klass,body_args)
